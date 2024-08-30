cab87663962a6c74c6be13ff84609c567bc5f6c7
TheMuno <143614176+TheMuno@users.noreply.github.com>
2024-08-30 10:20:58 +0300

    Add allow headers

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ const model = genAI.getGenerativeModel({
     generationConfig: { responseMimeType: "application/json" }
 });
 
-/*app.use(function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   // res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
   // res.end();
-});*/
+});
 
 /*app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
