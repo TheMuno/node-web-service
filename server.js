a4818eb94c149db7fe9dac6870b61b04acc9212a
TheMuno <143614176+TheMuno@users.noreply.github.com>
2024-09-13 14:01:31 +0300

    Update server code

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ app.listen(port, () => console.log(`Server has started on port: ${port}`));
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 const genAI = new GoogleGenerativeAI('AIzaSyBsQe9jWhtqx9Xgw0TNfipo2pErGpBp-vc');
 
-const model = genAI.getGenerativeModel({
-    model: 'gemini-1.5-flash',
-    // Set the `responseMimeType` to output JSON
-    generationConfig: { responseMimeType: 'application/json' }
-});
-
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   // res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
@@ -22,7 +16,7 @@ app.use(function(req, res, next) {
   // res.end();
 });
 
-// app.use(express.static('public')); // localhost
+app.use(express.static('public')); // localhost
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -30,18 +24,40 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', async (req, res) => {  
-  const { payload } = req.body;
+  const { payload, tryOut } = req.body;
+
   // console.log('payload', payload);
+  // console.log('tryOut', tryOut);
+
   if (!payload) {
       return res.status(400).send({ status: 'failed' });
   }
 
-  const geminiRes = await promptGemini(payload);
-  res.status(200).json(geminiRes);
+  const geminiRes = await promptGemini(payload, tryOut);
+
+  if (tryOut) {
+    res.status(200).send(geminiRes);
+  }
+  else {
+    res.status(200).json(geminiRes);
+  }
+  
 });
 
 // trigger gemini with the prompt 
-async function promptGemini(prompt) {
+async function promptGemini(prompt, tryOut=false) {
+    const modelObj = { model: 'gemini-1.5-flash' };
+
+    if (!tryOut) modelObj.generationConfig = { responseMimeType: 'application/json' };
+
+    /*const model = genAI.getGenerativeModel({
+        model: 'gemini-1.5-flash',
+        // Set the `responseMimeType` to output JSON
+        generationConfig: { responseMimeType: 'application/json' }
+    });*/
+
+    const model = genAI.getGenerativeModel(modelObj);
+
     const result = await model.generateContent(prompt);
     const response = await result.response;
 
